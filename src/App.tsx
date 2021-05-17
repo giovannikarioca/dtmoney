207a7703e38ca889f0be3a9c6e49e924ce267b7e
Giovanni Ramos <krkbjj@gmail.com>
2021-05-16 22:49:52 -0300

    create and style header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import styled from 'styled-components';
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
+
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 64px;`
+
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
   );
 }
