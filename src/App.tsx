4c3a944a39f6d2f52eb2ace16428e5397823dee4
Giovanni Ramos <krkbjj@gmail.com>
2021-05-16 23:26:05 -0300

    create and style summary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import styled from 'styled-components';
+import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
-
-const Title = styled.h1`
-  color: #8257e6;
-  font-size: 64px;`
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
