79cbdcede94432b8595b79d7cc0ec442009f9b99
Giovanni Ramos <krkbjj@gmail.com>
2021-05-22 03:36:05 -0300

    Form and style NewTransactionModal

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 Modal.setAppElement('#root');
 
@@ -10,23 +11,22 @@ export function App() {
 
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
 
-    function handleOpenNewTransactionModal() {
-        setIsNewTransactionModalOpen(true);
-    }
-    function handleCloseNewTransactionModal() {
-        setIsNewTransactionModalOpen(false);
-    }
-    
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}>
-        <h2>Cadastrar Transação</h2>
-      </Modal>
+        onRequestClose={handleCloseNewTransactionModal}
+      />
 
       <GlobalStyle />
     </>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-
 interface HeaderProps {
     onOpenNewTransactionModal: () => void;
 }
