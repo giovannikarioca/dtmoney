cfd9d46cead5a56cc7ff8ad8563b71ee79a39973
Giovanni Ramos <krkbjj@gmail.com>
2021-05-23 05:03:16 -0300

    colors in button

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.div`
     h2 {
@@ -59,6 +59,13 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
     isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+
+const colors = {
+    green: '#33cc95',
+    red: '#e52e4d'
+
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
@@ -67,7 +74,9 @@ export const RadioBox = styled.button<RadioBoxProps>`
         border: 1px solid #d7d7d7;
         border-radius: 0.25rem;
 
-        background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
+        background: ${(props) => props.isActive 
+        ? transparentize(0.9, colors[props.activeColor])
+        : 'transparent'};
 
         display: flex;
         align-items: center;
