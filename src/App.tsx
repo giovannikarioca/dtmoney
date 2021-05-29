d2202d71c606cb06472c443af52cb05091496876
giovannikarioca <krkbjj@gmail.com>
2021-05-29 13:35:25 -0300

    Project configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { Dashboard } from './components/Dashboard';
+import { useState } from 'react';
 import Modal from 'react-modal';
+import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
-import { useState } from 'react';
+
 import { NewTransactionModal } from './components/NewTransactionModal';
 
 Modal.setAppElement('#root');
